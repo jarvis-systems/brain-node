5f7c085bfe51bfaecb00126c98061e5f67cf8a36
xsaven <gms.sw.xsaven@gmail.com>
2025-12-27 07:40:13 +0200

    .env.example

---
+++ node/Commands/Task/AsyncCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BrainNode\Commands\Task;
+
+use BrainCore\Archetypes\CommandArchetype;
+use BrainCore\Attributes\Includes;
+use BrainCore\Attributes\Meta;
+use BrainCore\Attributes\Purpose;
+use BrainCore\Includes\Commands\Task\TaskAsyncInclude;
+
+#[Meta('id', 'task:async')]
+#[Meta('description', 'Async task execution command')]
+#[Purpose('Execute tasks asynchronously')]
+#[Includes(TaskAsyncInclude::class)]
+class AsyncCommand extends CommandArchetype
+{
+    /**
+     * Handle the command logic.
+     */
+    protected function handle(): void
+    {
+
+    }
+}
