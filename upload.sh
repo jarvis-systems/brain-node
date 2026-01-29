f85d3d27fe984468bd7c57141f634fd6ceb79d53
xsaven <gms.sw.xsaven@gmail.com>
2026-01-29 15:26:03 +0200

    upload.sh

--- upload.sh
+++ upload.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 API_KEY="jarvis:6c13d58df5771293f5825a50f10ee7005839735f"
+CURRENT_DIR=$(pwd)
 
 # -- Git Aliases --
 
@@ -21,21 +22,21 @@ curl -X POST -H'Content-Type:application/json' -H"Authorization: Bearer $API_KEY
 
 # -- Upload CLI to Packagist --
 
-GITHUB_REPO=$(cd cli && git config --get remote.origin.url | sed -e 's/.*github.com[:/]\(.*\)\.git/\1/')
+GITHUB_REPO=$(cd "$CURRENT_DIR/cli" && git config --get remote.origin.url | sed -e 's/.*github.com[:/]\(.*\)\.git/\1/')
 GITHUB_REPO="https://github.com/$GITHUB_REPO"
 
 echo $GITHUB_REPO
 
-cd cli && untag v0.0.1 && push -y && tag v0.0.1 -y
+cd "$CURRENT_DIR/cli" && untag v0.0.1 && push -y && tag v0.0.1 -y
 curl -X POST -H'Content-Type:application/json' -H"Authorization: Bearer $API_KEY" 'https://packagist.org/api/update-package' -d "{\"repository\":\"$GITHUB_REPO\"}"
 
 
 # -- Upload CORE to Packagist --
 
-GITHUB_REPO=$(cd core && git config --get remote.origin.url | sed -e 's/.*github.com[:/]\(.*\)\.git/\1/')
+GITHUB_REPO=$(cd "$CURRENT_DIR/core" && git config --get remote.origin.url | sed -e 's/.*github.com[:/]\(.*\)\.git/\1/')
 GITHUB_REPO="https://github.com/$GITHUB_REPO"
 
 echo $GITHUB_REPO
 
-cd core && untag v0.0.1 && push -y && tag v0.0.1 -y
+cd "$CURRENT_DIR/core" && untag v0.0.1 && push -y && tag v0.0.1 -y
 curl -X POST -H'Content-Type:application/json' -H"Authorization: Bearer $API_KEY" 'https://packagist.org/api/update-package' -d "{\"repository\":\"$GITHUB_REPO\"}"
