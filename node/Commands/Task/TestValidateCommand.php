27da28848e8a30b657bc7312f0a4e84bd9d7ea5f
xsaven <gms.sw.xsaven@gmail.com>
2026-01-31 20:39:58 +0200

    1/31/2026, 20:39:58

---
+++ node/Commands/Task/TestValidateCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BrainNode\Commands\Task;
+
+use BrainCore\Archetypes\CommandArchetype;
+use BrainCore\Attributes\Includes;
+use BrainCore\Attributes\Meta;
+use BrainCore\Attributes\Purpose;
+use BrainCore\Includes\Commands\Task\TaskTestValidateInclude;
+
+#[Meta('id', 'task:test-validate')]
+#[Meta('description', 'Test validate task execution command')]
+#[Purpose('Run validation test flow for a task.')]
+#[Includes(TaskTestValidateInclude::class)]
+class TestValidateCommand extends CommandArchetype
+{
+    /**
+     * Handle the command logic.
+     */
+    protected function handle(): void
+    {
+
+    }
+}
